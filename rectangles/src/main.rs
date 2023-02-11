c0be57332db36a5c851871fcf7017d676f41c01b
Sergio Calderón <66924320+sergiocarp10@users.noreply.github.com>
2023-02-11 14:12:56 -0300

    methods and constructor

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,6 +4,31 @@ struct Rectangle {
     height: u32
 }
 
+// struct methods
+impl Rectangle {
+    fn valid(&self) -> bool {
+        self.width > 0 && self.height > 0
+    }
+
+    fn can_hold(&self, another: &Rectangle) -> bool {
+        self.width > another.width && self.height > another.height
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+// struct constructors
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size
+        }
+    }
+}
+
 
 fn main() {
     // let width1 = 40;
@@ -16,19 +41,31 @@ fn main() {
         height: 50 
     };
 
+    // using constructor (by ::)
+    let rect2 = Rectangle::square(35);
+
     // show dimensions in one line
     println!("rect1 is {:?}", rect1);
 
     // show each attribute by line
     println!("rect1 is {:#?}", rect1);
 
+    // check dimensions
+    if rect1.valid() && rect2.valid() {
+        println!("both rectangles are valid");
+    }
+
     // calculate area
     println!(
         "The area of the rectangle is {} square pixels.",
-        area_v3(&rect1)
+        rect1.area()
     );
+
+    // compare rectangles
+    println!("can rect1 hold rect2? {}", rect1.can_hold(&rect2));
 }
 
+/*
 fn area_v1(width: u32, height: u32) -> u32 {
     width * height
 }
@@ -41,3 +78,4 @@ fn area_v2(dimensions: (u32, u32)) -> u32 {
 fn area_v3(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
+*/
